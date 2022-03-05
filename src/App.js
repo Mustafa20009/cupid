d2a6d17b2b8020eee8089444bb170ed687de9308
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2022-03-04 17:48:39 -0800

    from Zain subhani

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import About from './Component/About'
 import RoadMap from './Component/RoadMap'
 import Team from './Component/Team'
+import Main from './Component/Main'
 const App = () => {
   return (
     <div  >
+      <Main/>
       <About/>
       <About color="white" styleforFirstDiv={"order-2 pl-20 "} textWidth={"w-full"}styleForSecondDiv={"order-1"} />
       <About  />
--- src/Component/Team.jsx
+++ src/Component/Team.jsx
@@ -4,14 +4,14 @@ const Team = () => {
   return (
     <div className='grid gap-24 grid-cols-12 container mx-auto p-16' >
         <h1 className='font-semibold text-5xl col-span-12 ' >Team</h1>
-        <div className='col-span-4 rounded-full border-solid border-4 border-primary h-[50vh] ' >
+        <div className='col-span-4 rounded-full border-solid border-4 border-primary h-[55vh] ' >
             
 
         </div>
-        <div className='col-span-4 rounded-full border-solid border-4 border-primary h-[50vh] ' >
+        <div className='col-span-4 rounded-full border-solid border-4 border-primary h-[55vh] ' >
 
         </div>
-<div className='col-span-4 rounded-full border-solid border-4 border-primary h-[50vh] ' >
+<div className='col-span-4 rounded-full border-solid border-4 border-primary h-[55vh] ' >
 
 </div>
     </div>
